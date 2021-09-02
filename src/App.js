309528f00f74a46f7798ce6ccfd973a073874c1c
Jon Dawson <dawson.jon@hotmail.com>
2021-09-02 17:05:09 +0100

    cube updated with close controls

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
 import * as THREE from "three";
 import './style.css';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import gsap from 'gsap'
+import * as dat from 'dat.gui'
+
+console.log(OrbitControls)
 
 class App extends Component {
   componentDidMount() {
+
+    const gui = new dat.GUI({ closed: false, width: 400 })
+    gui.domElement.id = 'gui';
     
     // === THREE.JS CODE START ===
     const scene = new THREE.Scene();
@@ -18,26 +26,84 @@ class App extends Component {
     const cube = new THREE.Mesh( geometry, material );
     scene.add( cube );
 
-    // Camera 
-    const sizes = { 
-      width: window.innerWidth, // sets width to browser window
-      height: window.innerHeight
-    }   
+    const parameters = {
+      color: 0xff0000,
+      spin: () => {
+        gsap.to(cube.rotation, { duration: 1, y: cube.rotation.y + 10})
+      }
+    }
 
-    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-    camera.position.z = 5
+    // Debug 
+    gui
+    .add(cube.position, 'y')
+    .min(-3)
+    .max(3)
+    .step(0.01)
+    .name('elevation') 
+    // .name is an optional label 
+
+    gui
+    .add(cube, 'visible')
+    // creates a check box in close controls to make visible or invisible 
+
+    gui
+    .add(material, 'wireframe')
+    // create a wireframe version 
+
+    gui
+    .addColor(parameters, 'color')
+    .onChange(() => 
+    {
+    console.log(material.color.set(parameters.color))
+    })
+
+    gui
+    .add(parameters, 'spin')
+
+    // Sizes
+    const sizes = { 
+      width: window.innerWidth, 
+      height: window.innerHeight
+    }
+
+    window.addEventListener('resize', () =>
+    {
+        // Update sizes
+        sizes.width = window.innerWidth
+        sizes.height = window.innerHeight
+
+        // Update camera
+        camera.aspect = sizes.width / sizes.height
+        camera.updateProjectionMatrix()
+
+        // Update renderer
+        renderer.setSize(sizes.width, sizes.height)
+        renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    })
+
+    // Camera
+    const camera = new THREE.PerspectiveCamera( 75, sizes.width/sizes.height, 0.1, 1000 );
+    camera.position.z = 3
 
     // Render
     const renderer = new THREE.WebGLRenderer();
+    const canvas = renderer.domElement
     renderer.setSize( sizes.width, sizes.height );
-    document.body.appendChild( renderer.domElement );
+    document.body.appendChild( canvas );
+
+    const controls = new OrbitControls(camera, renderer.domElement)
+    controls.enableDamping = true
 
     // Animate 
     const animate = () => {
-      requestAnimationFrame( animate );
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
+     
+      // Orbit Controls 
+      controls.update()
+
+      // Render 
       renderer.render( scene, camera );
+      requestAnimationFrame( animate );
+      
     };
     animate();
     // === THREE.JS EXAMPLE CODE END ===
